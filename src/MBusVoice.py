ed4dff91beac3819891b8b4f290bebe1c8d0b388
Mike McGookey <mikemcgo@umich.edu>
2017-01-25 21:25:19 -0500

    Created one api wrapper function and intergrated it into the application Added factors that need to be considered in api functions/flow

--- src/MBusVoice.py
+++ src/MBusVoice.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 from flask_ask import Ask, statement
 
+from mbus_api_wrapper import *
+
 app = Flask(__name__)
 ask = Ask(app, '/')
 
@@ -11,8 +13,13 @@ def launch():
 
 @ask.intent('GetNextBusforStop')
 def hello(StopID):
-	print(StopID)
-	text = render_template('GetNextBusforStop', StopID=StopID)
+	estimated_times = get_buses_for_stop(StopID)[0]
+	options = {
+		'stopid': StopID,
+		'minutes': estimated_times['avg'],
+		'busline': routes[estimated_times['route']]
+	}
+	text = render_template('GetNextBusforStop', **options)
 	return statement(text).simple_card('GetNextBusforStop', text)
 
 if __name__ == '__main__':
