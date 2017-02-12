170636509b5a9cedf7d8edd52e0b43e1dd8ae4ec
Mike McGookey <mikemcgo@umich.edu>
2017-02-12 17:13:30 -0500

    Added stubs for all intents

--- src/intents.py
+++ src/intents.py
@@ -5,8 +5,8 @@ from flask_ask import Ask, statement
 blueprint = Blueprint("MBus_blueprint", __name__, url_prefix="/")
 ask = Ask(blueprint=blueprint)
 
-@ask.intent("GetNextBusesForStop")
-def getNextBusesForStop(StopID):
+@ask.intent("GetNextBusesForOrigin")
+def getNextBusesForOrigin(StopID):
 	bus_info = BusInfo([100, 597, 594, 596, 633])
 	eta = bus_info.get_eta(StopID).values()[0]
 	options = {
@@ -14,8 +14,20 @@ def getNextBusesForStop(StopID):
 		"minutes": eta.time,
 		"busline": bus_info.routes[eta.route].name
 	}
-	text = render_template('GetNextBusesForStop', **options)
-	return statement(text).simple_card('GetNextBusesForStop', text)
+	text = render_template('GetNextBusesForOrigin', **options)
+	return statement(text).simple_card('GetNextBusesForOrigin', text)
+
+@ask.intent("GetNextBusesForDestination")
+def getNextBusesForDestination(StopID):
+	return statement("Not Implemented")
+
+@ask.intent("SetFavorite")
+def setFavorite(StopName):
+	return statement("Not Implemented")
+
+@ask.intent("AssistedSetup")
+def assistedSetup():
+	return statement("Not Implemented")
 
 @ask.launch
 def launch():
