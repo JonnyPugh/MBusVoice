622ddb31eda33eaf00a2467a6601b8baa9e9373e
Mike McGookey <mikemcgo@umich.edu>
2017-02-19 23:04:08 -0500

    Added sessions to webapp Added routing logic to main.py to give error page when no alexaid is present

--- src/webapp/controllers/main.py
+++ src/webapp/controllers/main.py
@@ -5,10 +5,17 @@ main = Blueprint('main', __name__, template_folder='templates')
 
 @main.route('/')
 def index():
+	# Add the alexaID to the session if present, if not give error
 	alexaID = request.args.get('alexaID')
-	print stops
+	if alexaID:
+		if alexaID:
+			session['alexaID'] = alexaID
+		return redirect(url_for('main.index'))
+	if not alexaID and not 'alexaID' in session:
+		return "welcome to hell", 404
 	options = {
-		'alexaID': alexaID,
+		'alexaID': session['alexaID'],
 		'stop_names': stops
 	}
 	return render_template("index.html", **options)
+
