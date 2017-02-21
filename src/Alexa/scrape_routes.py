5332da7b46bfeb7f3c8ab3b91a2ca4c732cf0230
JonnyPugh <jpugh@umich.edu>
2017-02-20 21:27:28 -0500

    Updated scrape_routes to create a routes slot

--- src/Alexa/scrape_routes.py
+++ src/Alexa/scrape_routes.py
@@ -8,12 +8,14 @@ import os
 # are created in this directory no matter where the script is run
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 with open("interaction_model/routes.txt", "w") as routes_file:
-	routes_file.write("All Discovered Routes as of "+ctime()+":\n")
-	for route_id, route in BusInfo().routes.items():
-		routes_file.write("_____________________________________________________\n")
-		routes_file.write("Route name: "+route.name+"\n")
-		routes_file.write("ID: "+str(route_id)+"\n")
-		routes_file.write("Description: "+route.description+"\n")
-		routes_file.write("Start time: "+route.start_time+"\n")
-		routes_file.write("End time: "+route.end_time+"\n")
-		routes_file.write("Stops: "+str(route.stops)+"\n")
+	with open("interaction_model/routes_slot.txt", "w") as routes_slot:
+		routes_file.write("All Discovered Routes as of "+ctime()+":\n")
+		for route_id, route in BusInfo().routes.items():
+			routes_file.write("_____________________________________________________\n")
+			routes_file.write("Route name: "+route.name+"\n")
+			routes_file.write("ID: "+str(route_id)+"\n")
+			routes_file.write("Description: "+route.description+"\n")
+			routes_file.write("Start time: "+route.start_time+"\n")
+			routes_file.write("End time: "+route.end_time+"\n")
+			routes_file.write("Stops: "+str(route.stops)+"\n")
+			routes_slot.write(route.name.lower()+"\n")
