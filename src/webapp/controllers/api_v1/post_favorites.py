acaa7cb12bc0907b8e479694488f2200ac8139f0
Mike McGookey <mikemcgo@umich.edu>
2017-02-21 19:59:03 -0500

    Amended error checking to raise errors in api endpoints

--- src/webapp/controllers/api_v1/post_favorites.py
+++ src/webapp/controllers/api_v1/post_favorites.py
@@ -10,27 +10,24 @@ DESTINATION = "newDestination"
 def create_favorite(): 
 	req_json =  request.get_json()
 
-	malformed_request_error = verify_proper_request(req_json)
+	try:
+		verify_proper_request(req_json)
+		# Check stopname is in the api -- need access to api object
+			# on error, return error
 
-	if malformed_request_error is not None:
-		return malformed_request_error
+		# check if alias (if it exists) is distinct from system aliases/api stops -- need db access
+			# on error, return error
 
-	# Check stopname is in the api -- need access to api object
-		# on error, return error
+		# verify that user exists (can skip duplication checking) -- need db access
+			# If not, create and skip duplication prevention logic
 
-	# check if alias (if it exists) is distinct from system aliases/api stops -- need db access
-		# on error, return error
+		# verify new alias is not too similar to any other user aliases -- need db access
+			# if so, throw error
 
-	# verify that user exists (can skip duplication checking) -- need db access
-		# If not, create and skip duplication prevention logic
-
-	# verify new alias is not too similar to any other user aliases -- need db access
-		# if so, throw error
-
-	duplication_error = duplicate_entries(req_json['command_type'], req_json['stop_name'], req_json['alexa_id'])
-
-	if duplication_error is not None:
-		return duplication_error
+		# Check that the new stop isn't already in the list it is being added to	
+		duplicate_entries(req_json['command_type'], req_json['stop_name'], req_json['alexa_id'])
+	except RequestError as e:
+		return e.json, e.code
 
 	# check that the stop is not already a home or destination
 	# insert into db
@@ -45,22 +42,17 @@ def verify_proper_request(req_json):
 
 	parameter_error = verify_json_parameters(['stop_name', 'command_type', 'alexa_id', 'stop_alias'], req_json)
 
-	if parameter_error is not None:
-		return parameter_error
-
 	command_type = req_json['command_type']
 
 	if command_type == DESTINATION and not 'primary' in req_json:
-		return jsonify({"errors": [{"message": "No primary status specified"}]}), 400
+		raise BadRequest("No primary status specified")
 	elif command_type == ORIGIN and 'primary' in req_json:
-		return jsonify({"errors": [{"message": "No primary status can be specified for home stops"}]}), 400
+		raise BadRequest("No primary status can be specified for home stops")
 	elif command_type != DESTINATION and command_type != ORIGIN:
-		return jsonify({"errors": [{"message": "Invalid command type"}]}), 400
-
-	return None
+		raise BadRequest("Invalid command type")
 
 def duplicate_entries(command_type, stop_name, alexa_id):
-	return None
+	return 
 
 	
 def insert_db(req_json):
--- src/webapp/extensions.py
+++ src/webapp/extensions.py
@@ -1,7 +1,21 @@
 from flask import jsonify
 
+
+class RequestError(Exception):
+	def __init__(self, message, code):
+		self.code = code
+		self.json = jsonify({"errors": [{"message": message}]})
+		super(Exception, self).__init__(message)
+
+class BadRequest(RequestError):
+	def __init__(self, message):
+		super(RequestError, self).__init__(message, 400)
+
+class UnprocessableEntity(RequestError):
+	def __init__(self, message):
+		super(RequestError, self).__init__(message, 422)
+
 def verify_json_parameters(parameters, json):
     for parameter in parameters:
         if parameter not in json:
-            return jsonify({"errors": [{"message": "You did not provide the necessary fields"}]}), 400
-    return None
+            raise BadRequest("You did not provide the necessary fields")
