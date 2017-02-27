7a5e3c181db06670147399347d20a7a801aded1b
Mike McGookey <mikemcgo@umich.edu>
2017-02-27 16:30:44 -0500

    Can now edit database from edit_favorites endpoint There is a bug in the database wrapper pertaining to updating default_destinations to empty string

--- src/webapp/controllers/api_v1/edit_favorites.py
+++ src/webapp/controllers/api_v1/edit_favorites.py
@@ -1,4 +1,5 @@
 from extensions import *
+from quarantine import *
 from flask import *
 
 edit_favorites = Blueprint('edit_favorites', __name__, template_folder='templates')
@@ -10,13 +11,15 @@ def change_primary():
 	try:
 		verify_json_parameters(['alexa_id', 'stop_alias'], req_json)
 
-		# ensure user exists -- need db access
+		user_record = db.get_item(req_json['alexa_id'])
 
-		# ensure the user already has this alias set in the database -- need db access
+		if not req_json['stop_alias'] in user_record['destinations']:
+			raise BadRequest("Cannot change non existent destination to be primary")
 
 	except RequestError as e:
 		return e.json, e.code
-	# Update -- need db access
+	
+	db.update_item_field(req_json['alexa_id'], 'default_destination', user_record['destinations'][req_json['stop_alias']])
 
 	return jsonify(req_json), 200
 
@@ -27,19 +30,35 @@ def delete_favorite():
 	try:
 		malformed_request_error = verify_json_parameters(['alexa_id', 'stop_alias'], req_json)
 
-		# check that the user exists -- need db access
+		user_record = db.get_item(req_json['alexa_id'])
 
 		# check that this data exists in user home or destinations
 			# throw error if not
+		favorite_type = determine_favorite_type(req_json['stop_alias'], user_record)
+
 	except RequestError as e:
 		return e.json, e.code
 
-	# if deleting primary
-		# clear primary field
+	print user_record
+	if user_record[favorite_type][req_json['stop_alias']] == user_record['default_destination']:
+		# There appears to be a bug, becuse this causes a DatabaseError to be thrown
+		db.update_item_field(req_json['alexa_id'], 'default_destination', "")
 
-	# delete item from field
+	# Remove the alias from the user_record
+	user_record[favorite_type].pop(req_json['stop_alias'])
+	print user_record
+
+	db.update_item_field(req_json['alexa_id'], favorite_type, user_record[favorite_type])
 
 	return jsonify(alexa_id=req_json['alexa_id']), 200
+
+def determine_favorite_type(stop_alias, user_record):
+	if stop_alias in user_record['origins']:
+		return 'origins'
+	elif stop_alias in user_record['destinations']:
+		return 'destinations'
+	
+	raise BadRequest("Cannot delete non existent stop")
 
 
 
