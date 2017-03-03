05e8bb4a0548d4188fce5e4c3b78d174c96bf6bf
Malhar Pandhare <malharp@umich.edu>
2017-03-03 17:10:52 -0500

    checks if alexaID is in database before displaying main page. If it is not in the database, displays unauthorized.html instead

--- src/webapp/controllers/main.py
+++ src/webapp/controllers/main.py
@@ -1,3 +1,4 @@
+from quarantine import *
 from flask import *
 from stops import stops
 
@@ -6,16 +7,23 @@ main = Blueprint('main', __name__, template_folder='templates')
 @main.route('/')
 def index():
 	# Add the alexaID to the session if present, if not give error
+
 	alexaID = request.args.get('alexaID')
 	if alexaID:
-		if alexaID:
-			session['alexaID'] = alexaID
+		#check if the alexaID is in the db, should be added when card is first sent
+		#if not in db, do not show them main page
+		try:
+			record = db.get_item(alexaID)
+		except KeyError as e:
+			abort(403)
+
+		session['alexaID'] = alexaID
 		return redirect(url_for('main.index'))
 	if not alexaID and not 'alexaID' in session:
 		abort(403)
+
 	options = {
 		'alexaID': session['alexaID'],
 		'stop_names': stops,
 	}
 	return render_template("index.html", **options)
-
