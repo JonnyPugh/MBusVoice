2b4a26487762900a377637e7b360a3671ae34a17
JonnyPugh <jpugh@umich.edu>
2017-03-04 22:40:57 -0500

    Changed the Alexa skill to recognize user level aliases

--- src/Alexa/intents.py
+++ src/Alexa/intents.py
@@ -1,8 +1,6 @@
 from quarantine import *
 from flask import Blueprint, render_template
 from flask_ask import Ask, statement, session
-from difflib import get_close_matches
-from boto3 import resource
 
 blueprint = Blueprint("MBus_blueprint", __name__, url_prefix="/")
 ask = Ask(blueprint=blueprint)
@@ -21,33 +19,28 @@ def launch():
 		"NumBuses": 1
 	})
 def getNextBuses(StartStop, EndStop, RouteName, NumBuses):
-	# If parameters are None, get user values from the database
-	if not StartStop or not EndStop:
-	    item = resource("dynamodb", region_name="us-east-1").Table("UserFavorites").get_item(
-	        Key={
-				"AlexaID": str(session.user.userId)
-			}
-	    )["Item"]
-	    template = "MissingFavorite"
+	# Get user preferences from the database
+	user_info = db.get_item(session.user.userId)
+	template = "MissingFavorite"
 
 	try:
-		# Try to understand which stop the user is talking about
+		# Try to understand which stops the user is talking about
 		if StartStop:
-			StartStop, start_stops = shared.clarifyStopName(StartStop)
+			StartStop, start_stops = shared.clarifyStopName(StartStop, user_info["origins"])
 		else:
-			start_stops = item["origins"].values()
+			start_stops = user_info["origins"].values()
 			if not start_stops:
 				text = render_template(template, stopType="starting", favoriteType="home")
 				return statement(text).simple_card(template, text)
 		if EndStop:
-			EndStop, end_stops = shared.clarifyStopName(EndStop)
+			EndStop, end_stops = shared.clarifyStopName(EndStop, user_info["destinations"])
 		else:
-			stopID = item["primary"]
-			end_stops = [stopID]
-			if not end_stops:
-				text = render_template(template, stopType='ending', favoriteType='destination')
+			stopID = user_info["default_destination"]
+			if stopID == -1:
+				text = render_template(template, stopType="ending", favoriteType="destination")
 				return statement(text).simple_card(template, text)
-			EndStop = {stopid: alias for alias, stopid in item['destinations'].items()}[stopID]
+			end_stops = [stopID]
+			EndStop = {stopid: alias for alias, stopid in user_info["destinations"].items()}[stopID]
 	except shared.InvalidUserAlias as e:
 		return statement(e.message)
 
@@ -65,16 +58,20 @@ def getNextBuses(StartStop, EndStop, RouteName, NumBuses):
 				# route or if the route doesn't contain one of the 
 				# destination stops, don't use it as a possible eta
 				etas.append((stop_eta, start_stop))
-	etas = sorted(etas, key=lambda eta_info: eta_info[0].time)
 
 	# If there are no valid etas, return an error message
 	if not etas:
-		text = render_template("NoEtas", 
+		template = "NoEtas"
+		text = render_template(template, 
 			route=RouteName if RouteName else "", 
 			origin=StartStop if StartStop else "your home stops", 
 			destination=EndStop)
 		return statement(text).simple_card(template, text)
 
+	# Sort the etas by how soon their bus will arrive at their origin stop
+	etas = sorted(etas, key=lambda eta_info: eta_info[0].time)
+
+	# Form the response and return it to the user
 	options = {
 		"destination": EndStop
 	}
@@ -85,7 +82,7 @@ def getNextBuses(StartStop, EndStop, RouteName, NumBuses):
 		eta = eta_info[0]
 		stopID = eta_info[1]
 		if not StartStop:
-			StartStop = {stopid: alias for alias, stopid in item["origins"].items()}[stopID]	
+			StartStop = {stopid: alias for alias, stopid in user_info["origins"].items()}[stopID]	
 		options.update({
 			"origin": StartStop if len(start_stops) == 1 else bus_info.stops[stopID].name,
 			"route": bus_info.routes[eta.route].name,
--- src/Alexa/quarantine/shared.py
+++ src/Alexa/quarantine/shared.py
@@ -1,5 +1,5 @@
-from bus_info import *
-from database import *
+from bus_info import BusInfo
+from database import Database
 from data import stop_aliases
 from difflib import get_close_matches
 from flask import jsonify
@@ -20,7 +20,7 @@ class InvalidUserAlias(Exception):
 
 # Take the user's spoken stop name and figure out which stop 
 # id(s) they are referring to and return it/them as a list.
-def clarifyStopName(user_phrase):
+def clarifyStopName(user_phrase, favorite_stops):
 	# Use system level aliases and the stops given by the API and 
 	# see if the user was referring to one of those stops
 	user_phrase = user_phrase.lower()
@@ -30,8 +30,11 @@ def clarifyStopName(user_phrase):
 		return user_phrase, stop_aliases[user_phrase]
 
 	# If the specified stop name is similar to a user level alias,
-	# return all stop ids associated with that alias
-	user_aliases = {}
+	# return the stop id associated with that alias
+	close_matches = get_close_matches(user_phrase, favorite_stops.keys())
+	if close_matches:
+		name = close_matches[0]
+		return name, [favorite_stops[name]]
 
 	# If the specified stop name is similar to a system level alias or 
 	# a stop name, return all stop ids associated with it
