dfcda94db7b16d865a6ae90742a2ff843eaffe76
JonnyPugh <jpugh@umich.edu>
2017-04-03 21:14:33 -0400

    Changed name of stops API function

--- src/controllers/api_v1/stops.py
+++ src/controllers/api_v1/stops.py
@@ -4,6 +4,6 @@ from flask import Blueprint, jsonify
 stops_blueprint = Blueprint("stops_blueprint", __name__)
 
 @stops_blueprint.route("/api/v1/stops", methods=["GET"])
-def system_stops():
+def stops():
 	# Get a mapping of stopIDs to stop names for non-temporary stops
 	return jsonify({stopID: stop.name for stopID, stop in BusInfo().stops.items() if stop.name.split()[0] != "(TEMP)"})
