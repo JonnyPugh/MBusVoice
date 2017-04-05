3c50b55d49ecc613b2b7bf506e87b8bd7096f5f8
Mike McGookey <mikemcgo@umich.edu>
2017-04-05 00:01:04 -0400

    Started UI Overhaul Ajax requests complete Time is rendering Group nicknames and stop lists are rendering

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,34 +1,106 @@
+var cachedRecord = {};
+var stopIdToName;
+var nameToStopId = {};
+var numAjax = 6;
+
 document.addEventListener('DOMContentLoaded', function () {
 
+	$.get(apiUrl + "stops", function(data) {
+		stopIdToName = data;
+		for (var key in data) {
+			nameToStopId[data[key]] = key;
+		}
+	});
+	$.get(apiUrl + userId + "/time", function(data) {
+		cachedRecord["time"] = data["time"];
+	});
+	$.get(apiUrl + userId + "/home", function(data) {
+		cachedRecord["home"] = data["home"];
+	});
+	$.get(apiUrl + userId + "/destination", function(data) {
+		cachedRecord["destination"] = data["destination"];
+	});
+	$.get(apiUrl + userId + "/groups", function(data) {
+		cachedRecord["groups"] = data["groups"];
+	});
+	$.get(apiUrl + userId + "/order", function(data) {
+		cachedRecord["order"] = data["order"];
+	});
+
+	$(document).ajaxStop(function() {
+		$(this).unbind("ajaxStop");
+		renderUserPreferences();
+	});
 	// Add ajax call wrappers to onclick events for submit buttons
-	document.getElementById('homeSubmit').onclick = function() { postHome(); };
-	document.getElementById('destinationSubmit').onclick = function() { postDestination(); };
-	document.getElementById('submitChangePrimary').onclick = function() { postChangePrimary(); };
-	document.getElementById('submitDeleteFavorite').onclick = function() { postDeleteFavorite(); };
+	// document.getElementById('homeSubmit').onclick = function() { postHome(); };
+	// document.getElementById('destinationSubmit').onclick = function() { postDestination(); };
+	// document.getElementById('submitChangePrimary').onclick = function() { postChangePrimary(); };
+	// document.getElementById('submitDeleteFavorite').onclick = function() { postDeleteFavorite(); };
 
-	// Disable submit buttons for new home and destination stops
-	document.getElementById('homeSubmit').setAttribute('disabled', 'disabled')
-	document.getElementById('destinationSubmit').setAttribute('disabled', 'disabled')
+	// // Disable submit buttons for new home and destination stops
+	// document.getElementById('homeSubmit').setAttribute('disabled', 'disabled')
+	// document.getElementById('destinationSubmit').setAttribute('disabled', 'disabled')
 
-	populateSystemStops()
+	// populateSystemStops()
 
-	// Add event handlers to submit buttons to monitor for valid stop name
-	$('#homeStopName').bind('keyup input', function() {
-		disableInvalidSubmission('homeStopName', 'homeSubmit'); 
-	});
-	$('#destinationStopName').bind('keyup input', function() {
-		disableInvalidSubmission('destinationStopName', 'destinationSubmit'); 
-	});
+	// // Add event handlers to submit buttons to monitor for valid stop name
+	// $('#homeStopName').bind('keyup input', function() {
+	// 	disableInvalidSubmission('homeStopName', 'homeSubmit'); 
+	// });
+	// $('#destinationStopName').bind('keyup input', function() {
+	// 	disableInvalidSubmission('destinationStopName', 'destinationSubmit'); 
+	// });
 
-	renderUserFavorites();
 });
 
-function populateSystemStops() {
-	var listOptions = document.getElementsByClassName('systemLevelStop');
-	systemLevelStops = [];
-	for (var i = 0; i < listOptions.length; i++) {
-		 systemLevelStops.push(listOptions[i].value);
+function renderUserPreferences() {
+	$(".preferences").empty();
+
+	renderButton("Edit Preferences", enableEditMode);
+
+	document.getElementById("time").innerHTML = cachedRecord["time"] + " minutes";
+
+	renderGroup(cachedRecord["home"], "home");
+	renderGroup(cachedRecord["destination"], "destination");
+	for (var i = 0; i < cachedRecord["order"].length; i++) {
+		renderGroup(cachedRecord["order"][i], "groups");
 	}
+}
+
+function renderButton(displayText, func) {
+	var button = document.createElement("a");
+	button.innerHTML = displayText;
+	button.className = "btn btn-primary btn-lg";
+	button.onclick = func;
+	document.getElementById("buttons").appendChild(button);
+}
+
+function renderGroup(nickname, divId) {
+	// Render nickname, or indicate unpopulated group
+	var div = document.createElement("div");
+	div.className = "list-group";
+
+	if (nickname == null) {
+		nickname = "This group has not been set";
+		return
+	}
+	// Do not show the user a populated nickname and instead show an error if nickname is null
+	var nicknameElement = document.createElement("a");
+	nicknameElement.innerHTML = nickname;
+	nicknameElement.className = "list-group-item active";
+	div.appendChild(nicknameElement);
+	for (var i = 0; i < cachedRecord["groups"][nickname].length; i++) {
+		var listElement = document.createElement("a");
+		listElement.innerHTML = stopIdToName[cachedRecord["groups"][nickname][i]];
+		listElement.className = "list-group-item"
+		div.appendChild(listElement)
+	}
+
+	document.getElementById(divId).appendChild(div);
+}
+
+function enableEditMode() {
+
 }
 
 function formPost(form, formData) {
